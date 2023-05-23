788a9840aaccab4fa24eb5824d6e890dd12e06c6
Ignacio Ampuero <7380952+iampuero@users.noreply.github.com>
2023-05-23 11:58:31 -0400

    setup changes

--- setup.py
+++ setup.py
@@ -20,6 +20,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+          'numpy',
+          'numba',
+          'scipy'
+    ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
